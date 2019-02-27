27904e19a31e953ef755a2efa18031922e3ad455
stap780 <panaet80@mail.ru>
2019-02-27 20:16:03 +0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root :to => "home#index"
+  root :to => "homes#index"
 
 end
